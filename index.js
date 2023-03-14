cb44665dc48bc3e6486c9c42b1ca3e41f09eb2c6
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-03-14 03:49:02 -0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
+
 const character = newImage('assets/green-character/static.gif')
+
+let direction = null;
+let x = 100;
+let y = 250;
+
+function moveCharacter(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
 move(character).to(100, 250)
+//console.log(direction = 'east')
+
+
+setInterval(moveCharacter, 1)  
+
+  
 
 
 move(newImage('assets/tree.png')).to(200, 450)
