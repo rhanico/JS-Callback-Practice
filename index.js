f4e723667bdd82b2bd8129d9c06131245b619c51
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-03-14 03:52:22 -0700

    added code to js file/ move file''

--- index.js
+++ index.js
@@ -28,8 +28,30 @@ function moveCharacter(){
 move(character).to(100, 250)
 //console.log(direction = 'east')
 
-
 setInterval(moveCharacter, 1)  
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+
+
 
   
 
