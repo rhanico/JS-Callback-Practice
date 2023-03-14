24f66e5b0d35ef36f81f3f91786163e68800ba99
FRANZ RHANICO <109740904+rhanico@users.noreply.github.com>
2023-03-14 04:18:30 -0700

    fixed/doubed check code

--- index.js
+++ index.js
@@ -4,56 +4,30 @@ move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
 
-let direction = null;
-let x = 100;
-let y = 250;
-
-function moveCharacter(){ 
+function handleDirectionChange(directionChange){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
     if(direction === 'west'){
-        x = x - 1
+        character.src = 'assets/green-character/west.gif'
     }
     if(direction === 'north'){
-        y = y + 1
+        character.src = 'assets/green-character/north.gif'
     }
     if(direction === 'east'){
-        x = x + 1
+        character.src = 'assets/green-character/east.gif'
     }
     if(direction === 'south'){
-        y = y - 1
+        character.src = 'assets/green-character/south.gif'
     }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
 }
 
-move(character).to(100, 250)
-//console.log(direction = 'east')
-
-setInterval(moveCharacter, 1)  
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
 
-  
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
